409c5e86065095629ff0a5926c42bb540e6d1a7a
vntvibr <vitor.brina@venturus.org.br>
2021-03-08 13:15:51 -0300

    Project using react-three-viewer lib.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useViewer } from "react-three-viewer";
 
-function App() {
+import "./App.css";
+
+const App: React.FC = () => {
+  const [binds, { load }] = useViewer();
+
+  const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = event.target.files;
+
+    if (!fileList) return;
+
+    const file = fileList[0];
+
+    if (!file) return;
+
+    load(file);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Upload the STL file that is in Project's Root</h1>
+        <input type="file" onChange={handleUpload} />
+        <div>
+          <canvas ref={binds} />
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
